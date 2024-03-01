4ae59c417552b3701966d1a842710955303e844b
Giovandro Santos <vsm.giovandro@gmail.com>
2024-02-29 21:19:27 -0300

    HTML e CSS para componente CHAMADO-LIST

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Chamado} from "../../models/chamado";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-chamado-list',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoListComponent implements OnInit {
 
-  constructor() { }
+  ELEMENT_DATA: Chamado[] = []
+  FILTERED_DATA: Chamado[] = []
+
+  displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
+  dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+      //private service: ChamadoService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  orderByStatus(number: number) {
+    
+  }
+
+  retornaPrioridade(prioridade: any) {
+    return "";
+  }
+
+  retornaStatus(status: number | string) {
+    return "";
+  }
 }
